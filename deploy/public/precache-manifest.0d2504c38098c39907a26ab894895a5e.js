ec6d5de622640a84e387c9cedccbbaac3bfa630a
“jdnunez” <jdnunez@indracompany.com>
2022-03-09 08:47:04 -0500

    deploy

---
+++ deploy/public/precache-manifest.0d2504c38098c39907a26ab894895a5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4779e729f6aa0d28636874231b9edcf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a9712a3ebbc6f25e69",
+    "url": "/static/css/main.8e4d5216.chunk.css"
+  },
+  {
+    "revision": "dbed5290fdc7ef8e2d5f",
+    "url": "/static/js/2.785e48fc.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/static/js/2.785e48fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a9712a3ebbc6f25e69",
+    "url": "/static/js/main.1005ef70.chunk.js"
+  },
+  {
+    "revision": "c4957e584f3aeb929d78",
+    "url": "/static/js/runtime-main.4b6cd884.js"
+  },
+  {
+    "revision": "20fb69cfeaa05e30a0423fe454dcf4f4",
+    "url": "/static/media/face.20fb69cf.jpeg"
+  }
+]);
